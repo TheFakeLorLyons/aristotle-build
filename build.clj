8dd25951d6510a13ba09da34b4c2bb8c4c9a8f17
Lor <lorelailyons@protonmail.com>
2025-02-25 13:15:44 -0500

    Added clojars deployment information to deps and build.clj

--- build.clj
+++ build.clj
@@ -12,24 +12,54 @@
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
 
 (defn clean [_]
-  (println "Doing the clean")
   (b/delete {:path "target"}))
 
 (defn jar [_]
-  (println "Building the jar.")
   (b/write-pom {:class-dir class-dir
                 :lib lib
                 :version version
                 :basis basis
-                :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src"] :target-dir class-dir})
-  (b/jar {:class-dir class-dir :jar-file jar-file}))
+                :src-dirs ["src"]
+                :scm {:url "https://github.com/TheFakeLorLyons/aristotle-build"
+                      :connection "scm:git:git://github.com/TheFakeLorLyons/aristotle-build" 
+                      :tag (b/git-process {:git-args "rev-parse HEAD"})}
+                :pom-data [[:description "Description of your library"]
+                           [:url "https://github.com/TheFakeLorLyons/aristotle-build"]
+                           [:licenses
+                            [:license
+                             [:name "Eclipse Public License 2.0"]
+                             [:url "https://www.eclipse.org/legal/epl-2.0/"]]]
+                           [:developers
+                            [:developer
+                             [:name "Melody"]
+                             [:email "wistfulmelodylispfulprogram@gmail.com"]]]]})
 
-;;; :basis - required, used for :mvn/local-repo
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
 (defn install [_]
   (println "Installing: class-dir =" class-dir "version = " version)
   (let [opts {:lib lib :basis basis :jar-file jar-file :class-dir class-dir :version version}]
     (b/install opts)))
 
+(defn deploy [_]
+  (println "Deploying version" version "to Clojars")
+  (b/deploy {:class-dir class-dir
+             :lib lib
+             :version version
+             :basis basis
+             :jar-file jar-file
+             :repository "clojars"
+             :sign-releases? false}))
+
 (defn all-jar [_]
-  (clean nil) (jar nil) (install nil))
+  (clean nil) 
+  (jar nil) 
+  (install nil))
+
+(defn release [_]
+  (clean nil)
+  (jar nil)
+  (deploy nil))
